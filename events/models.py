54c6f105b51141abf14acb54eb49483a9dff18ee
Maulik-Ujeniya <228997825+Maulik-Ujeniya@users.noreply.github.com>
2026-07-15 19:29:33 +0530

    Add Event model with ManyToMany volunteers relationship

---
+++ events/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from volunteers.models import Volunteer
+
+# Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    dscription = models.TextField(blank=True)
+    date = models.DateField()
+    budget_allocated = models.DecimalField(max_digits=10,decimal_places=2,default=0)
+    budget_used = models.DecimalField(max_digits=10,decimal_places=2,default=0)
+    Volunteers = models.ManyToManyField(Volunteer,blank=True) 
+
+    def __str__(self):
+        return self.name
--- hopebridge/settings.py
+++ hopebridge/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'donors', 
     'volunteers', 
     'donations',
+    'events',
 ]
 
 MIDDLEWARE = [
