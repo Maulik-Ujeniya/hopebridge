6fabc52673f553c5baf8dcf41afc3d2eb6789ff3
Maulik-Ujeniya <228997825+Maulik-Ujeniya@users.noreply.github.com>
2026-07-16 12:56:07 +0530

    Add donor detail page with donation history

--- donors/urls.py
+++ donors/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.donor_list, name='donor_list'),
+    path('<int:donor_id>/', views.donor_detail, name='donor_detail'),
 ]
--- donors/views.py
+++ donors/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Donor
+
 # Create your views here.
 
 def donor_list(request):
     donors = Donor.objects.all()
     return render(request, 'donors/donor_List.html',{'donors': donors}) 
+
+def donor_detail(request, donor_id):
+    donor = get_object_or_404(Donor, id=donor_id)
+    donations = donor.donation_set.all()
+    return render(request, 'donors/donor_detail.html', {'donor': donor, 'donations': donations})
 
